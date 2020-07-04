0579e7ad3101df5172ec30217bc8b2621eb87a86
Andrei Jiroh Eugenio Halili <andreijiroheugeniohalili24680@gmail.com>
2020-07-04 08:57:34 +0000

    [string-generator] Updated the string generator script to send the string to the target account's Saved Messages.

--- string_generator.py
+++ string_generator.py
@@ -1,10 +1,22 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
-print("""Please go-to my.telegram.org
-Login using your Telegram account
-Click on API Development Tools
-Create a new application, by entering the required details""")
-APP_ID = int(input("Enter APP ID here: "))
-API_HASH = input("Enter API HASH here: ")
+
+print("""==============================
+String Session Generator
+
+You are about to generate an string sessions that works
+
+If you don't have both API ID and hash for Telegram, please get one from https://my.telegram.org.
+==============================
+""")
+
+APP_ID = int(input("[PROMPT] Enter your APP ID here: "))
+API_HASH = input("[PROMPT] Enter your API HASH here: ")
+
+# Copied from https://github.com/SpEcHiDe/UniBorg/blob/master/GenerateStringSession.py#L19
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
-    print(client.session.save())
+    session_string = client.session.save()
+    saved_messages_template = """Here's your <code>STRING_SESSION</code>: <code>{}</code>
+
+⚠️ <i>It is forbidden to pass it to third-parties.</i>""".format(session_string)
+    client.send_message("me", saved_messages_template, parse_mode="html")

a3142b8819aab8dbf1c3abb2e37d01d4a4423aa9
Andrei Jiroh Eugenio Halili <andreijiroheugeniohalili24680@gmail.com>
2020-07-20 10:06:59 +0000

    [repo/wip] Grammar fixes on Main module. Press F.
    Thanks to Blankie in https://t.me/PublicSafetyBureau/8492 for help!
    
    Signed-off-by: Andrei Jiroh Eugenio Halili <andreijiroheugeniohalili24680@gmail.com>

--- Sibyl_System/plugins/main_manager.py
+++ Sibyl_System/plugins/main_manager.py
@@ -100,7 +100,7 @@ async def approve(event):
             # checks to not gban the Gbanner and find who is who
             if reply == me.id:
                 list = re.findall(r'tg://user\?id=(\d+)', replied.text)
-                reason = re.search(r"(\*\*)?Reuqest Reason:(\*\*)? (`([^`]*)`|.*)", replied.text)
+                reason = re.search(r"(\*\*)?Request Reason:(\*\*)? (`([^`]*)`|.*)", replied.text)
                 reason = reason.group(4) if reason.group(4) else reason.group(3)
                 if len(list) > 1:
                     id1 = list[0]

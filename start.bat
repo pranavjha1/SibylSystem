771c6a9cc9192d11724e06982ae994182f22ec24
The Pins Team <yourfriends@madebythepins.tk>
2020-09-05 19:01:27 +0800

    [cli] Update the CLI source code.
    Also in this commit, I added the start script for Windows users.
    
    Signed-off-by: Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
 TITLE Launch SSC Userbot
-env\scripts\activate.bat && python -m Sibyl_System
+env\scripts\activate.bat && echo "If you run this on startup, please do not close or the bot will be go offline." && python -m Sibyl_System
